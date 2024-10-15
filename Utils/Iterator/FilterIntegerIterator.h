b17322d3c70addb25886fee7a63bb01459c111ba
Arkadi Davtyan <arkadi.davtyan.17@gmail.com>
2024-10-15 23:46:37 +0400

    fixing mitakes

--- Utils/Iterator/FilterIntegerIterator.h
+++ Utils/Iterator/FilterIntegerIterator.h
@@ -1,13 +1,13 @@
 #ifndef FILTER_INTEGER_ITERATOR_H
 
 #define FILTER_INTEGER_ITERATOR_H
-
+#include <functional>
+#include <iostream>
+#include <utility>
 class FilterIntegerIterator {
 public:
-    FilterIntegerIterator(int *data, int size, bool (*condition)(int));
+    FilterIntegerIterator(int *data, int size, std::function<bool(int)> other_condition);
     FilterIntegerIterator(int *data, int size, int *ptr);
-    ~FilterIntegerIterator();
-
     FilterIntegerIterator begin();
     FilterIntegerIterator end();
 
@@ -19,36 +19,32 @@ private:
     int *ptr_m;
     int size_m;
     int *data_m;
+    std::function<bool(int)> condition;
 };
-FilterIntegerIterator::FilterIntegerIterator(int *data, int size, bool (*condition)(int))
-        : ptr_m(nullptr), size_m(0) , data_m(new int[size]) {
-    int k = 0;
-    for (int i = 0; i < size; ++i) {
-        if (condition(data[i])) {
-            data_m[k++] = data[i];
-            size_m++;
-        }
-    }
-}
+FilterIntegerIterator::FilterIntegerIterator(int *data, int size, std::function<bool(int)> other_condition)
+        : ptr_m(data), size_m(size) , data_m(data) , condition(std::move(other_condition))
+{}
 FilterIntegerIterator::FilterIntegerIterator(int *data, int size, int *ptr)
-        : size_m(size), data_m(new int[size]), ptr_m(ptr) {}
-
-FilterIntegerIterator::~FilterIntegerIterator() {
-    delete[] data_m;
-}
+        : size_m(size), data_m(data), ptr_m(ptr) {}
 
 FilterIntegerIterator FilterIntegerIterator::begin() {
-    return FilterIntegerIterator(data_m, size_m, data_m);
+    FilterIntegerIterator tmp(data_m, size_m, data_m);
+    tmp.condition = condition;
+    while(!condition(*tmp.ptr_m) && tmp.ptr_m < tmp.ptr_m + tmp.size_m) {
+        tmp.ptr_m++;
+    }
+    return tmp;
 }
-
 FilterIntegerIterator FilterIntegerIterator::end() {
-    return FilterIntegerIterator(data_m, size_m, data_m + size_m);
+    FilterIntegerIterator tmp(data_m, size_m, data_m + size_m );
+    tmp.condition = condition;
+    return tmp;
 }
-
 void FilterIntegerIterator::operator++() {
-    ptr_m++;
+    do {
+        ++ptr_m;
+    } while (ptr_m != data_m + size_m && !condition(*ptr_m));
 }
-
 bool FilterIntegerIterator::operator!=(const FilterIntegerIterator &other) {
     return this->ptr_m != other.ptr_m;
 }
