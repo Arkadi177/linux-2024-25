ad374106045881b7bd13d8f951a9b67843c54617
Arkadi Davtyan <arkadi.davtyan.17@gmail.com>
2024-10-20 12:58:44 +0400

    Added newline etc

--- Utils/Iterator/CircularIntIterator.h
+++ Utils/Iterator/CircularIntIterator.h
@@ -4,26 +4,14 @@ class CircularIntIterator{
     int size_m;
     int* ptr_m;
 public:
-    CircularIntIterator(int* , int);
-    void operator++();
-    bool operator!=(const CircularIntIterator&);
-    int& operator*() const;
+    CircularIntIterator(int* data, int size) : size_m(size) , data_m(data) , ptr_m(data) {}
+    void operator++() {
+        ptr_m = (ptr_m == size_m + data_m - 1)? data_m : ptr_m + 1;
+    }
+    bool operator!=(const CircularIntIterator& other) {
+        return this->ptr_m != other.ptr_m;
+    }
+    int& operator*() const {
+        return *ptr_m;
+    }
 };
-CircularIntIterator::CircularIntIterator(int* data , int size)
-        : size_m(size) , data_m(data) , ptr_m(data){
-}
-void CircularIntIterator::operator++() {
-    if(ptr_m == size_m + data_m - 1)
-    {
-        ptr_m = data_m;
-    }
-    else {
-        ptr_m++;
-    }
-}
-bool CircularIntIterator::operator!=(const CircularIntIterator &other) {
-    return this->ptr_m != other.ptr_m;
-}
-int &CircularIntIterator::operator*() const {
-    return *ptr_m;
-}
--- Utils/Iterator/FilterIntegerIterator.h
+++ Utils/Iterator/FilterIntegerIterator.h
@@ -1,54 +1,47 @@
-#ifndef FILTER_INTEGER_ITERATOR_H
-
-#define FILTER_INTEGER_ITERATOR_H
+#pragma once
 #include <functional>
-#include <iostream>
 #include <utility>
 class FilterIntegerIterator {
 public:
-    FilterIntegerIterator(int *data, int size, std::function<bool(int)> other_condition);
-    FilterIntegerIterator(int *data, int size, int *ptr);
-    FilterIntegerIterator begin();
-    FilterIntegerIterator end();
 
-    void operator++();
-    bool operator!=(const FilterIntegerIterator &other);
-    int &operator*() const;
+    FilterIntegerIterator(int *data, int size, std::function<bool(int)> other_condition) : ptr_m(data), size_m(size) , data_m(data)
+    , condition(std::move(other_condition)) {}
+
+    FilterIntegerIterator(int *data, int size, int *ptr) : size_m(size), data_m(data), ptr_m(ptr)  {}
+
+    FilterIntegerIterator begin() {
+        FilterIntegerIterator tmp(data_m, size_m, data_m);
+        tmp.condition = condition;
+        while(!condition(*tmp.ptr_m) && tmp.ptr_m < tmp.ptr_m + tmp.size_m) {
+            tmp.ptr_m++;
+        }
+        return tmp;
+    }
+
+    FilterIntegerIterator end() {
+        FilterIntegerIterator tmp(data_m, size_m, data_m + size_m );
+        tmp.condition = condition;
+        return tmp;
+    }
+
+    void operator++() {
+        do {
+            ++ptr_m;
+        } while (ptr_m != data_m + size_m && !condition(*ptr_m));
+    }
+
+    bool operator!=(const FilterIntegerIterator &other) {
+        return this->ptr_m != other.ptr_m;
+    }
+
+    int &operator*() const {
+        return *ptr_m;
+    }
 
 private:
     int *ptr_m;
     int size_m;
     int *data_m;
     std::function<bool(int)> condition;
-};
-FilterIntegerIterator::FilterIntegerIterator(int *data, int size, std::function<bool(int)> other_condition)
-        : ptr_m(data), size_m(size) , data_m(data) , condition(std::move(other_condition))
-{}
-FilterIntegerIterator::FilterIntegerIterator(int *data, int size, int *ptr)
-        : size_m(size), data_m(data), ptr_m(ptr) {}
 
-FilterIntegerIterator FilterIntegerIterator::begin() {
-    FilterIntegerIterator tmp(data_m, size_m, data_m);
-    tmp.condition = condition;
-    while(!condition(*tmp.ptr_m) && tmp.ptr_m < tmp.ptr_m + tmp.size_m) {
-        tmp.ptr_m++;
-    }
-    return tmp;
-}
-FilterIntegerIterator FilterIntegerIterator::end() {
-    FilterIntegerIterator tmp(data_m, size_m, data_m + size_m );
-    tmp.condition = condition;
-    return tmp;
-}
-void FilterIntegerIterator::operator++() {
-    do {
-        ++ptr_m;
-    } while (ptr_m != data_m + size_m && !condition(*ptr_m));
-}
-bool FilterIntegerIterator::operator!=(const FilterIntegerIterator &other) {
-    return this->ptr_m != other.ptr_m;
-}
-int &FilterIntegerIterator::operator*() const {
-    return *ptr_m;
-}
-#endif // FILTER_INTEGER_ITERATOR_H
+};
