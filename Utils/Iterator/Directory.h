4973b17aa2f58448d2ea3680a0e7f75edde6ee56
Arkadi Davtyan <arkadi.davtyan.17@gmail.com>
2024-10-22 21:12:05 +0400

    Simplifying the code

--- Utils/Iterator/Directory.h
+++ Utils/Iterator/Directory.h
@@ -1,85 +1,99 @@
 #pragma once
-#include <iostream>
-#include <vector>
-#include <stack>
 #include <dirent.h>
+#include <iostream>
+#include <stack>
 
-std::string vectorToString(const std::vector<std::string>& vec, const std::string& separator = "/") {
-    std::string resultStr;
-    for (size_t i = 0; i < vec.size(); ++i) {
-        resultStr += vec[i];
-        if (i != vec.size() - 1) {
-            resultStr += separator;
-        }
-    }
-    return resultStr;
-}
+struct DirectoryDFSNodeData
+{
+  std::string m_basename;
+  off_t m_offset;
+};
 
 class Directory {
-    std::string path;
-    DIR *dir;
-public:
-    class Recursive_directory_iterator {
-        std::vector<std::string> path;
-        std::string file_name;
-        dirent *entry;
-        DIR *dir;
-        std::stack<long> positions;
-    public:
-        Recursive_directory_iterator(const std::string& file_name , dirent* entry , DIR* dir) : file_name(file_name), dir(dir) , entry(entry){
-            if(!file_name.empty()) {
-                path.push_back(file_name);
-            }
-        }
-        Recursive_directory_iterator& operator=(const Recursive_directory_iterator& other) {
-            if (this == &other) {
-                return *this;
-            }
-            dir = other.dir;
-            file_name = other.file_name;
-            entry = other.entry;
-            return *this;
-        }
-        void operator++() {
-            if(entry != nullptr && entry->d_type == DT_DIR && std::string(entry->d_name) !=  "." && std::string(entry->d_name) != "..") {
-                path.emplace_back(entry->d_name);
-                positions.emplace(telldir(dir));
-                dir = opendir(vectorToString(path).c_str());
-            }
-            entry = readdir(dir);
-            if(entry == nullptr && path.size() > 1) {
-                path.pop_back();
-                dir = opendir(vectorToString(path).c_str());
-                seekdir(dir , positions.top());
-                positions.pop();
-                entry = readdir(dir);
-            }
-        }
-        bool operator==(const Recursive_directory_iterator& other) const{
-            if(other.entry == entry) {
-                return true;
-            }
-            return false;
-        }
-        bool operator!=(const Recursive_directory_iterator& other) const {
-            return !(*this == other);
-        }
-        char* operator*()const {
-            return entry->d_name;
-        }
-    };
 
-    explicit Directory(std::string& path) : path(path) {
-        dir = opendir(path.c_str());
+  std::string path;
+  DIR* dir;
+
+ public:
+
+  class RecursiveDirectoryIterator{
+
+    std::string m_file_name;
+    dirent* m_entry;
+    DIR* m_dir;
+    std::stack<DirectoryDFSNodeData> m_stack;
+
+   public:
+
+    RecursiveDirectoryIterator(const std::string& file_name, dirent* entry,
+                                 DIR* dir)
+        : m_file_name(file_name), m_dir(dir), m_entry(entry) {
+      if (!file_name.empty()) {
+        m_stack.push({file_name, 0});
+      }
     }
-    Recursive_directory_iterator begin() const {
-        dirent* entry = readdir(dir);
-        return {path, entry, dir};
+
+    RecursiveDirectoryIterator& operator=(
+        const RecursiveDirectoryIterator& other) {
+      if (this == &other) {
+        return *this;
+      }
+      m_dir = other.m_dir;
+      m_file_name = other.m_file_name;
+      m_entry = other.m_entry;
+      return *this;
     }
-    Recursive_directory_iterator end() const {
-        return {"", nullptr, dir};
+
+    void operator++() {
+      if (m_entry != nullptr && m_entry->d_type == DT_DIR &&
+          std::string(m_entry->d_name) != "." &&
+          std::string(m_entry->d_name) != "..") {
+          m_stack.push({m_stack.top().m_basename + "/" + m_entry->d_name, telldir(m_dir)});
+          m_dir = opendir(m_stack.top().m_basename.c_str());
+      }
+      m_entry = readdir(m_dir);
+      if (m_entry == nullptr && m_stack.size() > 1) {
+          off_t off = m_stack.top().m_offset;
+          m_stack.pop();
+          m_dir = opendir(m_stack.top().m_basename.c_str());
+          seekdir(m_dir , off);
+          m_entry = readdir(m_dir);
+      }
+
     }
-    ~Directory() {
-        closedir(dir);
+
+    bool operator==(const RecursiveDirectoryIterator& other) const {
+      if (other.m_entry == m_entry) {
+        return true;
+      }
+      return false;
     }
+
+    bool operator!=(const RecursiveDirectoryIterator& other) const {
+      return !(*this == other);
+    }
+
+    char* operator*() const {
+      return m_entry->d_name;
+    }
+
+  };
+
+  explicit Directory(std::string& path) : path(path) {
+    dir = opendir(path.c_str());
+  }
+
+  RecursiveDirectoryIterator begin() const {
+    dirent* entry = readdir(dir);
+    return {path, entry, dir};
+  }
+
+  RecursiveDirectoryIterator end() const {
+    return {"", nullptr, dir};
+  }
+
+  ~Directory() {
+    closedir(dir);
+  }
+
 };
