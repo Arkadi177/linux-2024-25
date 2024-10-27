3f285b82da7445f551208b9b35b3f7ff17f32880
Arkadi Davtyan <arkadi.davtyan.17@gmail.com>
2024-10-27 15:04:50 +0400

    Writing do_magic in different transaction unit:

--- Duplication_Spell/do_magic.cpp
+++ Duplication_Spell/do_magic.cpp
@@ -1,13 +1,6 @@
 #include <iostream>
-#include <fcntl.h>
-#include <unistd.h>
-
-void do_magic() {
-  int fd = open("../../Duplication_Spell/new_pts.txt" , O_RDONLY);
-  dup2(fd , STDIN_FILENO);
-  close(fd);
-}
-
+#include "imlement_of_magic.h"
+void do_magic();
 int main()
 {
 
