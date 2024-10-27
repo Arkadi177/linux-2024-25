5687a57da99627efc5bbc41b49326c6b333fe9ee
Arkadi Davtyan <arkadi.davtyan.17@gmail.com>
2024-10-27 15:10:19 +0400

    using std::string_view

--- Duplication_Spell/diff_fd_write.cpp
+++ Duplication_Spell/diff_fd_write.cpp
@@ -8,10 +8,10 @@ int main()
 
   int fd1 = open("../../Duplication_Spell/exclusive_file.log", O_WRONLY);
   int fd2 = dup(fd1);
-  const char* first_line = "First line:\n";
-  const char* second_line = "Second line: ";
-  write(fd1, first_line, strlen(first_line));
-  write(fd2, second_line, strlen(second_line));
+  std::string_view first_line = "First line:\n";
+  std::string_view second_line = "Second line: ";
+  write(fd1, first_line.data(), first_line.size());
+  write(fd2, second_line.data(), second_line.size());
   close(fd1);
   close(fd2);
   return 0;
