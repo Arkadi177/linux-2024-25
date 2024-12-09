d0a4af299ae7e6ef81ccffaa29edee7afad4c4e3
Arkadi Davtyan <arkadi.davtyan.17@gmail.com>
2024-12-09 22:09:33 +0400

    refactoring allocator:

--- Utils/Allocator/my_allocator.h
+++ Utils/Allocator/my_allocator.h
@@ -1,47 +1,65 @@
-#ifndef MY_ALLOCATOR_H
-#define MY_ALLOCATOR_H
+#pragma once
 #include <iostream>
-#include <new>
-#include <ostream>
 
-template <class T>
-class MyAllocator {
-  public:
-    using value_type = T;
+class FixedBufferAllocator {
+private:
+    struct Block {
+        std::size_t block_size;
+        bool isFree;
+        Block* next;
+    };
+    std::byte* buffer;
+    std::size_t size;
+    Block* freeList;
 
-    MyAllocator() = default;
+public:
+    FixedBufferAllocator(void* buf, std::size_t bufSize)
+        : buffer(static_cast<std::byte*>(buf)), size(bufSize), freeList(nullptr) {
+        freeList = reinterpret_cast<Block*>(buffer);
+        freeList->block_size = bufSize - sizeof(Block);
+        freeList->isFree = true;
+        freeList->next = nullptr;
+    }
+    void* allocate(std::size_t chunkSize) {
+        if (chunkSize == 0 || chunkSize > size - sizeof(Block)) {
+            throw std::bad_alloc();
+        }
 
-    template <class U>
-    explicit MyAllocator(const MyAllocator<U>& rhs) {}
+        Block* current = freeList;
+        Block* prev = nullptr;
 
-    T* allocate(size_t size) {
-      T* mem = static_cast<T*>(::operator new(size * sizeof(T)));
-      if(mem == nullptr) {
-        std::cerr << "Out of memory!" << std::endl;
+        while (current != nullptr) {
+            if (current->isFree == true && current->block_size >= chunkSize) {
+                if (current->block_size > chunkSize + sizeof(Block)) {
+                    Block* newBlock = reinterpret_cast<Block*>(
+                        reinterpret_cast<std::byte*>(current) + sizeof(Block) + chunkSize
+                    );
+                    newBlock->block_size = current->block_size - chunkSize - sizeof(Block);
+                    newBlock->isFree = true;
+                    newBlock->next = current->next;
+                    current->block_size = chunkSize;
+                    current->next = newBlock;
+                }
+                current->isFree = false;
+                if (prev != nullptr) {
+                    prev->next = current->next;
+                } else {
+                    freeList = current->next;
+                }
+                return reinterpret_cast<void*>(current + 1);
+            }
+
+            prev = current;
+            current = current->next;
+        }
+
         throw std::bad_alloc();
-      }
-      return mem;
     }
-    void deallocate(T* p , size_t size) {
-      ::operator delete(p);
-    }
+    void deallocate(void* ptr) {
+        Block* block = reinterpret_cast<Block*>(static_cast<std::byte*>(ptr) - sizeof(Block));
+        block->isFree = true;
 
-    bool operator==(const MyAllocator<T>& rhs) const {
-      return true;
+        block->next = freeList;
+        freeList = block;
     }
-    bool operator!=(const MyAllocator<T>& rhs) const {
-      return false;
-    }
-
 };
-template <class T , class U>
-bool operator==(const MyAllocator<T>& lhs , const MyAllocator<U>& rhs) {
-  return true;
-}
-
-template <class T , class U>
-bool operator!=(const MyAllocator<T>& lhs , const MyAllocator<U>& rhs) {
-  return false;
-}
-
-#endif //MY_ALLOCATOR_H
