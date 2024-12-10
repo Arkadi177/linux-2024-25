cde699677167056c0c0fb33a72fb5d38ca903b73
Arkadi Davtyan <arkadi.davtyan.17@gmail.com>
2024-12-10 22:00:49 +0400

    Xargs done:

--- Demo/Do_Command/do_command_xargs.cpp
+++ Demo/Do_Command/do_command_xargs.cpp
@@ -27,8 +27,20 @@ void do_command(const std::string& command_to_do) { // find it in man exec execl
   }
 }
 
-int main() {
+void xargs(int argc , char *argv[]) {
+  if (argc < 2) {
+    std::cerr << "Usage: xargs [filename]" << std::endl;
+    exit(EXIT_FAILURE);
+  }
   std::string command_to_do;
   getline(std::cin, command_to_do);
+  for (int i = 1; i < argc; i++) {
+    command_to_do.append(std::string(argv[i]));
+    command_to_do.push_back(' ');
+  }
   do_command(command_to_do);
+}
+
+int main(int argc , char *argv[]) {
+  xargs(argc, argv);
 }
