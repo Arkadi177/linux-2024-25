178cd48cac7d43d010c78ebe787db9e439bc681c
Arkadi Davtyan <arkadi.davtyan.17@gmail.com>
2024-12-13 23:52:01 +0400

    Moving to Utils:

--- Demo/Do_Command/do_command.cpp
+++ Demo/Do_Command/do_command.cpp
@@ -5,27 +5,7 @@
 #include <sys/wait.h>
 #include <cstring>
 #include <vector>
-
-void do_command(const std::string& command_to_do) { // find it in man exec execl("/bin/sh", "sh", "-c", command, (char *) NULL);
-  auto child = fork();
-  if (child == 0) {
-    execl("/bin/sh" , "sh" , "-c" ,command_to_do.c_str() , nullptr);
-    exit(EXIT_SUCCESS);
-  }else {
-    int status;
-    wait(&status);
-    if(WIFEXITED(status))
-    {
-      std::cout << "Process " << child << " exited with exit code " << WIFEXITED(status) <<"\n";
-    }
-    else if (WIFSIGNALED(status))
-    {
-      std::cout << "Process " << child << " was killed with signal " << WTERMSIG(status) << "\n";
-    }
-    std::cout << "Child process exited with status " << status << std::endl;
-    exit(EXIT_SUCCESS);
-  }
-}
+#include "do_command.h"
 
 int main() {
   std::string command_to_do;
