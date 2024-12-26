4f08ed6036888e676f140940338da99898a38865
Arkadi Davtyan <arkadi.davtyan.17@gmail.com>
2024-12-26 14:50:19 +0400

    Separating do_command adn xargs:

--- Demo/Xargs/do_command_xargs.cpp
+++ Demo/Xargs/do_command_xargs.cpp
@@ -5,17 +5,25 @@
 #include <sys/wait.h>
 #include <cstring>
 #include <vector>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <cstring>
+#include <string>
 
-void do_command(const std::string& command_to_do) { // find it in man exec execl("/bin/sh", "sh", "-c", command, (char *) NULL);
-  auto child = fork();
+int do_command(const std::string& command_to_do) {
+  pid_t child = fork();
+
   if (child == 0) {
-    execl("/bin/sh" , "sh" , "-c" ,command_to_do.c_str() , nullptr);
-    exit(EXIT_SUCCESS);
-  }else {
+    execl("/bin/sh", "sh", "-c", command_to_do.c_str(), nullptr);
+    return -1;
+  } else if (child > 0) {
     int status;
-    waitpid(-1, &status, 0);
-    exit(EXIT_SUCCESS);
+    waitpid(child, &status, 0);
+    return WEXITSTATUS(status);
   }
+
+  return -1;
 }
 
 void xargs(int argc , char *argv[]) {
