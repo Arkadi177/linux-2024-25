c309cce79773b05ec5020006dbe2195a37f1dd15
Arkadi Davtyan <arkadi.davtyan.17@gmail.com>
2024-12-26 14:43:25 +0400

    fixing issues:

--- Utils/Do_Command/do_command.h
+++ Utils/Do_Command/do_command.h
@@ -2,22 +2,24 @@
 #define DO_COMMAND_H
 
 #include <unistd.h>
-#include <iostream>
-#include <filesystem>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <cstring>
-#include <vector>
+#include <string>
 
-int do_command(const std::string& command_to_do) { // find it in man exec execl("/bin/sh", "sh", "-c", command, (char *) NULL);
-  auto child = fork();
+int do_command(const std::string& command_to_do) {
+  pid_t child = fork();
+
   if (child == 0) {
-    execl("/bin/sh" , "sh" , "-c" ,command_to_do.c_str() , nullptr);
+    execl("/bin/sh", "sh", "-c", command_to_do.c_str(), nullptr);
     return -1;
-  }else {
+  } else if (child > 0) {
     int status;
-    waitpid(-1, &status, 0);
+    waitpid(child, &status, 0);
     return WEXITSTATUS(status);
   }
+
+  return -1;
 }
-#endif //DO_COMMAND_H
+
+#endif // DO_COMMAND_H
