c29832c3c79d2211a8a42664d71d823836837fa0
Arkadi Davtyan <arkadi.davtyan.17@gmail.com>
2025-03-01 22:23:05 +0400

    Fixing issues:

--- Utils/Thread/BlockingQueue.h
+++ Utils/Thread/BlockingQueue.h
@@ -1,103 +1,89 @@
 #ifndef BLOCKING_QUEUE_H
 #define BLOCKING_QUEUE_H
+
 #include <condition_variable>
 #include <queue>
 #include <iostream>
-
-std::vector<bool> m_dead;
-int i = 0;
+#include <optional>
 
 template <typename T>
-class BlockingQueue{
+class BlockingQueue
+{
+    std::mutex m_mutex;
+    std::condition_variable m_condition_variable;
+    std::queue<T> m_queue;
+    std::size_t m_size = 0;
 
-  std::mutex m_mutex;
-  std::condition_variable m_condition_variable;
-  std::queue<T> m_queue;
-  std::size_t m_size;
-  int m_index;
-
-  public:
-
-   explicit BlockingQueue(std::size_t size)
-   : m_size(size) , m_index(i) {
-     m_dead.push_back(false);
-   }
-
-    void push(const T& item) {
-       std::unique_lock<std::mutex> lock(m_mutex);
-       m_condition_variable.wait(lock, [&]{return m_queue.size() < m_size;});
-       if(m_dead[m_index]) {
-         return;
-       }
-       m_queue.push(item);
-       lock.unlock();
-       m_condition_variable.notify_one();
-    }
-
-    void push(T&& item) {
-       std::unique_lock<std::mutex> lock(m_mutex);
-       m_condition_variable.wait(lock, [&]{return m_queue.size() < m_size;});
-       if(m_dead[m_index]) {
-         return;
-       }
-       m_queue.push(std::move(item));
-       lock.unlock();
-       m_condition_variable.notify_one();
-    }
-
-    bool try_push(const T& item) {
-       std::unique_lock<std::mutex> lock(m_mutex);
-       if(m_dead[m_index]) {
-         return false;
-       }
-       else if(m_queue.size() < m_size) {
-         m_queue.push(item);
-       }
-       return m_queue.size() < m_size;
-    }
-
-    bool try_push(T&& item) {
+public:
+    void set_size(std::size_t size)
+    {
         std::unique_lock<std::mutex> lock(m_mutex);
-        if(m_dead[m_index]) {
-          return false;
-        }
-        else if(m_queue.size() < m_size) {
-          m_queue.push(std::move(item));
+        m_size = size;
+    }
+
+    void push(const T& item)
+    {
+        std::unique_lock<std::mutex> lock(m_mutex);
+        m_condition_variable.wait(lock, [&] { return m_queue.size() < m_size; });
+        m_queue.push(item);
+        lock.unlock();
+        m_condition_variable.notify_one();
+     }
+
+    void push(T&& item)
+    {
+        std::unique_lock<std::mutex> lock(m_mutex);
+        m_condition_variable.wait(lock, [&] { return m_queue.size() < m_size; });
+        m_queue.push(std::move(item));
+        lock.unlock();
+        m_condition_variable.notify_one();
+    }
+
+    bool try_push(const T& item)
+    {
+        std::unique_lock<std::mutex> lock(m_mutex);
+        if (m_queue.size() < m_size)
+        {
+           m_queue.push(item);
         }
         return m_queue.size() < m_size;
     }
 
-    T pop() noexcept{
-       std::unique_lock<std::mutex> lock(m_mutex);
-       T item = {};
-       m_condition_variable.wait(lock , [&]{return !m_queue.empty();});
-       if(m_dead[m_index]) {
-         return item;
-       }
-       item = std::move(m_queue.front());
-       m_queue.pop();
-       lock.unlock();
-       m_condition_variable.notify_one();
-       return item;
+    bool try_push(T&& item)
+    {
+        std::unique_lock<std::mutex> lock(m_mutex);
+        if (m_queue.size() < m_size)
+        {
+           m_queue.push(std::move(item));
+        }
+        return m_queue.size() < m_size;
     }
 
-    bool try_pop(T& item) {
-       std::unique_lock<std::mutex> lock(m_mutex);
-       if(m_dead[m_index] || m_queue.empty()) {
-         return false;
-       }
-       item = std::move(m_queue.front());
-       m_queue.pop();
-       return true;
+    std::optional<T> pop()
+    {
+        std::unique_lock<std::mutex> lock(m_mutex);
+        m_condition_variable.wait(lock, [&] { return !m_queue.empty(); });
+        std::optional<T> result;
+        if(std::is_assignable<T , T>::value) {
+            result = m_queue.front();
+        }else {
+            result = std::nullopt;
+        }
+        m_queue.pop();
+        lock.unlock();
+        m_condition_variable.notify_one();
+        return result;
     }
 
-    ~BlockingQueue() {
-       std::unique_lock<std::mutex> lock(m_mutex);
-       m_dead[m_index] = true;
-       i++;
-       m_condition_variable.notify_all();
+    bool try_pop(T& item)
+    {
+        std::unique_lock<std::mutex> lock(m_mutex);
+        item = std::move(m_queue.front());
+        m_queue.pop();
+        return true;
     }
 
+    ~BlockingQueue() = delete;
 };
 
-#endif //BLOCKING_QUEUE_H
+#endif // BLOCKING_QUEUE_H
--- Utils/Thread/ThreadPool.h
+++ Utils/Thread/ThreadPool.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "BlockingQueue.h"
 #include <type_traits>
 #include <functional>
@@ -7,59 +8,72 @@
 #include <thread>
 #include <tuple>
 
-class ThreadPool{
-  template <class F , class... Args>
-  using return_type = typename std::invoke_result_t<F, Args...>;
+class ThreadPool
+{
+    template <class F, class... Args>
+    using return_type = typename std::invoke_result_t<F, Args...>;
 
-  std::vector<std::thread> m_threads;
-  BlockingQueue<std::function<void()>> m_tasks;
-  std::atomic<bool> m_stop;
-  std::atomic<std::size_t> m_thread_count;
+    std::vector<std::thread> m_threads;
+    BlockingQueue<std::function<void()>>* m_tasks;
+    std::atomic<bool> m_stop;
+    std::atomic<std::size_t> m_thread_count;
 
 public:
-  explicit ThreadPool(): m_tasks(std::thread::hardware_concurrency()),
-  m_stop(false), m_thread_count(std::thread::hardware_concurrency()) {
-    for(size_t i = 0; i < m_thread_count; i++) {
-      m_threads.emplace_back([this]{
-        while(true) {
-          std::function<void()> task = [this]{ return; };
-          if(task) {
-            task = m_tasks.pop();
-            task();
-          }else {
-            m_stop = true;
-          }
-          if(m_stop) {
-            break;
-          }
+    explicit ThreadPool()
+        : m_stop(false),
+          m_thread_count(std::thread::hardware_concurrency()),
+          m_tasks(new BlockingQueue<std::function<void()>>())
+    {
+        m_tasks->set_size(m_thread_count);
+        for (size_t i = 0; i < m_thread_count; i++)
+        {
+            m_threads.emplace_back([this]
+            {
+                while (!m_stop)
+                {
+                    auto task = m_tasks->pop();
+                    if (task.value())
+                    {
+                        task.value()();
+                    }
+                    else
+                    {
+                        m_stop = true;
+                    }
+                }
+            });
         }
-      });
     }
-  }
 
-  ~ThreadPool() {
-    m_stop = true;
-    for (int i = 0; i < m_thread_count; i++) {
-      m_tasks.push([]{});
+    ~ThreadPool()
+    {
+        m_stop = true;
+        for (int i = 0; i < m_thread_count; i++)
+        {
+            m_tasks->push(nullptr);
+        }
+        for (auto& t : m_threads)
+        {
+            if (t.joinable())
+            {
+                t.join();
+            }
+        }
+        operator delete(m_tasks);
     }
-    for(auto& t : m_threads) {
-      if(t.joinable()) {
-        t.join();
-      }
-    }
-  }
 
-  template <class F, class... Args>
+    template <class F, class... Args>
     auto enqueue(F&& f, Args&&... args) -> std::future<return_type<F, Args...>>
-  {
-    auto promise = std::make_shared<std::promise<return_type<F, Args...>>>();
-    auto future = promise->get_future();
+    {
+        auto promise = std::make_shared<std::promise<return_type<F, Args...>>>();
+        auto future = promise->get_future();
 
-    auto task = [promise, f = std::forward<F>(f), args = std::make_tuple(std::forward<Args>(args)...)]{
-      promise->set_value(std::apply(f , args));
-    };
+        auto task = [promise, f = std::forward<F>(f), args = std::make_tuple(std::forward<Args>(args)...)]()
+        {
+            promise->set_value(std::apply(f, args));
+        };
 
-    m_tasks.push(std::move(task));
-    return future;
-  }
+        m_tasks->push(std::move(task));
+        return future;
+    }
 };
